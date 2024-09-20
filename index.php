45c9d0aa23ba3b59ffe6c01d1e7965ed14a3efb1
Amunra <hungnguyentrong285@gmail.com>
2024-09-21 00:52:45 +0700

    Up

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$hung = 'I love Ha <br>';
+echo $hung;
+
+
+// Debug Data
+// var_dump($hung);
+
+$arr = [1, 2, 3, 4];
+
+// print_r($arr);
+
+// Ghép 
+$ha = 'Yes, I same';
+echo 'Hung said:' . $hung . '<br>' . 'Ha said:' . $ha;
+
+//Hằng số
+define('_PI', 3.14);
+const SO_PI = 3.14;
+
+//Ép kiểu
+$bien1 = 'true';
+echo is_bool($bien1);
