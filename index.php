17d2ede57da3ccc416f587163d6bf16cfdba8d0f
Amunra <hungnguyentrong285@gmail.com>
2024-09-24 23:11:01 +0700

    Up

--- index.php
+++ index.php
@@ -36,5 +36,31 @@ echo $bien11 === $bien12;
 echo 'dừng bên dưới';
 exit('Đã dừng');
 
-echo('Dừng thật chưa');
+// echo('Dừng thật chưa');
 
+// Alternative syntax for control structures in PHP
+
+// Alternative syntax for if statement
+if ($condition):
+    // Code to execute if condition is true
+elseif ($anotherCondition):
+    // Code to execute if another condition is true
+else:
+    // Code to execute if none of the conditions are true
+endif;
+
+// Alternative syntax for for loop
+for ($i = 0; $i < 10; $i++):
+    // Code to execute in each iteration
+endfor;
+
+// Alternative syntax for while loop
+while ($condition):
+    // Code to execute while condition is true
+endwhile;
+
+// Alternative syntax for foreach loop
+foreach ($array as $key => $value):
+    // Code to execute for each element in the array
+endforeach;
+?>
---
+++ require.php
@@ -0,0 +1,7 @@
+<?php
+    // include'index.php';// Dùng để import file vào file hiện tại nếu lỗi thì các câu lệnh bên dưới vẫn chạy
+    // include_once'index.php';
+    // require'index.php';// Dùng để import file vào file hiện tại nếu lỗi thì các câu lệnh bên dưới sẽ không chạy
+    // require_once'index.php';
+
+?>
