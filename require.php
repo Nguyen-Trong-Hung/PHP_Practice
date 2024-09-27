6d134c102e818c51ca072755ce0aee913ebb29d1
Amunra <hungnguyentrong285@gmail.com>
2024-09-27 23:43:12 +0700

    Up

--- require.php
+++ require.php
@@ -4,4 +4,16 @@
     // require'index.php';// Dùng để import file vào file hiện tại nếu lỗi thì các câu lệnh bên dưới sẽ không chạy
     // require_once'index.php';
 
+    // Sự khác biệt giữa print_r và echo trong PHP
+
+    // echo
+    // echo là một cấu trúc ngôn ngữ trong PHP được sử dụng để xuất một hoặc nhiều chuỗi ra màn hình.
+    // echo không thể xuất các kiểu dữ liệu phức tạp như mảng hoặc đối tượng một cách trực tiếp.
+    echo "Hello, World!"; // Xuất chuỗi "Hello, World!" ra màn hình
+
+    // print_r
+    // print_r là một hàm trong PHP được sử dụng để xuất thông tin về một biến theo cách dễ đọc.
+    // print_r có thể xuất các kiểu dữ liệu phức tạp như mảng hoặc đối tượng.
+    $array = array('foo' => 'bar', 'baz' => 'qux');
+    print_r($array); // Xuất thông tin về mảng theo cách dễ đọc
 ?>
--- string .php
+++ string .php
@@ -34,3 +34,55 @@ echo str_replace('World', 'Thien Ha', $chuoi);
 
 // Hàm mã hóa chuỗi theo md5
 echo md5($chuoi).'<br>';
+
+// Hàm mã hóa chuỗi theo sha1
+echo sha1($chuoi).'<br>';
+
+// in ra hiển thị thẻ html
+echo htmlentities('<h1>Thien Ha</h1>');
+
+// Chuyển định dạng thực thể html sang định dạng HTML ban đầu
+echo html_entity_decode(htmlentities('<h1>Thien Ha</h1>'));
+
+// Hàm loại bỏ các thẻ html khỏi chuỗi
+echo strip_tags('<h1>Thien Ha</h1>');
+
+// Hàm cắt chuỗi
+echo substr($chuoi, 0, 5);
+
+// Tách một chuỗi từ một ký tự nào đó
+echo strtok($chuoi2, ',');
+
+// Hàm cắt chuỗi theo từ
+echo strstr($chuoi2, 'World');
+
+// Tìm vị trí của chuỗi
+echo strpos($chuoi2, 'World');
+
+// Vừa cắt chuỗi vừa thay thế chuỗi
+echo substr_replace($chuoi2, 'Thien Ha', 0, 5);
+
+// Chuyển đổi chữ hoa thành chữ thường và ngược lại
+strtolower($chuoi);
+strtoupper($chuoi);
+
+// Chuyển chữ cái đầu tiên thành chữ hoa và ngược lại (Upercase and Lowercase)
+ucfirst($chuoi);
+lcfirst($chuoi);
+
+// Chuyển chữ cái đầu tiên của tất cả các từ thành chữ hoa
+ucwords($chuoi);
+
+// Loại bỏ khoảng trắng ở đầu và cuối chuỗi
+trim('   Hello World   ');
+// Loại bỏ chữ cái H ở đầu và cuối chuỗi
+echo trim($chuoi, 'H');
+
+$arr = [1, 2, 3, 4];
+
+// Chuyển chuỗi ở dạng json thành array hoặc object
+$jsondecode = json_decode(json_encode($arr));
+print_r($jsondecode);
+// Chuyển array hoặc object thành chuỗi json
+json_encode($arr);
+
